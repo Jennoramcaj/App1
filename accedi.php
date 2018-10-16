d19602b4bb363f9a92745a44a0a867fc55747530
nomeutente <mailutente@gmail.com>
2018-10-16 12:27:32 +0200

    aaaaaaa

--- accedi.php
+++ accedi.php
@@ -8,7 +8,7 @@
 		<div id='shakeBox'>
 			<fieldset> <!-- serve a creare una linea di contorno -->
 				<legend>Login:</legend>
-				
+
 					<label>
 						Username: <input type="text" name="username" maxlength="30" /> <input type="submit" value="login" name="loginSubmit" />
 					</label>
