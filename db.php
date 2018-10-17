ab4296021794c26185aeb34214776930de50b027
nomeutente <mailutente@gmail.com>
2018-10-17 14:28:40 +0200

    ciao

--- db.php
+++ db.php
@@ -3,10 +3,10 @@
 	$dbname="db_primo";
 	$dbuser="root";
 	$dbpass="";
-$conn=mysql_connect($dbhost, $dbuser, $dbpass) or die ("impossibile connettersi al DB");
+$conn=mysql_connect($dbhost, $dbuser, $dbpass) or die ("impossibile connettersi al DB ciao");
 	mysql_select_db($dbname, $conn) or die ("impossibile connettersi a $dbname");
 	session_start();
-				
-	
-	
+
+
+
 	?>
